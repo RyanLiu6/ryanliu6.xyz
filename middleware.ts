351ff92931b6e099f85e030b64716857e7f10bcc
Ryan Liu <ryan@ryanliu6.xyz>
2024-02-12 13:53:17 -0800

    Updating middleware logging to be more useful

--- middleware.ts
+++ middleware.ts
@@ -16,12 +16,12 @@ export const config = {
 export default function middleware(request: NextRequest) {
   let hostname = request.headers.get("host")!;
 
-  console.log(hostname)
+  console.log(`Hostname: ${hostname}`)
 
   // If hostname is either localhost or public domain, then we just redirect to root
   if (hostname === "localhost:3000" || hostname === process.env.NEXT_PUBLIC_ROOT_DOMAIN) {
     let path = request.nextUrl.pathname;
-    console.log(path);
+    console.log(`Regular Pattern found, directing to ${path}`);
     if(path === "/") {
         return NextResponse.rewrite(new URL(path, request.url));
     } else {
@@ -29,7 +29,8 @@ export default function middleware(request: NextRequest) {
     }
   }
 
-  console.log(hostname.split(".")[0])
   // Else, we direct whatever.domain.com to domain.com/whatever
-  return NextResponse.rewrite(new URL(`/${hostname.split(".")[0]}`, request.url));
+  let path = hostname.split(".")[0]
+  console.log(`Rewriting to ${path}`)
+  return NextResponse.rewrite(new URL(`/${path}`, request.url));
 }
