982b4f12659be9b54ef020b26c2a872415ecd9d2
Ryan Liu <ryan@ryanliu6.xyz>
2024-02-12 00:43:33 -0800

    Adding valentines day code and middleware for rerouting domain/x -> x.domain

---
+++ app/valentine/page.tsx
@@ -0,0 +1,94 @@
+/*
+The base code is directly from `https://gist.github.com/tnarla/0c09a11fea366145ba684fe6ebf578c5`.
+Parts of it have been modified to better fit my purposes, including the usage of a Conditional component.
+*/
+
+"use client";
+
+import { useState } from "react";
+import { Conditional } from "../utils/utils";
+
+export default function Page() {
+  const [noCount, setNoCount] = useState(0);
+  const [yesPressed, setYesPressed] = useState(false);
+  const [loop, setLoop] = useState(0);
+
+  const phrases = [
+    "No",
+    "Are you sure?",
+    "Really sure?",
+    "Think again!",
+    "Last chance!",
+    "Surely not?",
+    "You might regret this!",
+    "Give it another thought!",
+    "Are you absolutely certain?",
+    "This could be a mistake!",
+    "Have a heart!",
+    "Don't be so cold!",
+    "Change of heart?",
+    "Wouldn't you reconsider?",
+    "Is that your final answer?",
+    "You're breaking my heart ;(",
+  ];
+
+  let yesButtonSize = 0;
+
+  if (loop > 0) {
+    yesButtonSize = (noCount + loop * phrases.length) * 20 + 16;
+  } else {
+    yesButtonSize = noCount * 20 + 16;
+  }
+
+  const handleNoClick = () => {
+    setNoCount(noCount + 1);
+  };
+
+  const getNoButtonText = () => {
+    if (noCount >= phrases.length) {
+      setLoop(loop + 1);
+      setNoCount(0);
+    }
+
+    return phrases[noCount];
+  };
+
+  function Yay() {
+    return (
+      <>
+      <img src="https://media.tenor.com/gUiu1zyxfzYAAAAi/bear-kiss-bear-kisses.gif" />
+      <div className="text-4xl font-bold my-4">Ok yay!!!</div>
+      </>
+    )
+  }
+
+  function No() {
+    return (
+      <>
+      <img className="h-[200px]" src="https://gifdb.com/images/high/cute-love-bear-roses-ou7zho5oosxnpo6k.gif" />
+      <h1 className="text-4xl my-4">Will you be my Valentine?</h1>
+      <div>
+        <button
+          className={`bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded mr-4`}
+          style={{ fontSize: yesButtonSize }}
+          onClick={() => setYesPressed(true)}
+        >
+          Yes
+        </button>
+        <button
+          onClick={handleNoClick}
+          className=" bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
+        >
+          {noCount === 0 ? "No" : getNoButtonText()}
+        </button>
+      </div>
+    </>
+    )
+  }
+
+  return (
+    <div className="flex flex-col items-center justify-center h-screen -mt-16">
+      <Conditional show={yesPressed} trueChildren={<Yay/>} falseChildren={<No/>}/>
+    </div>
+  );
+}
---
+++ middleware.ts
@@ -0,0 +1,32 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export const config = {
+  matcher: [
+    /*
+    * Match all request paths except for the ones starting with:
+    * - api (API routes)
+    * - _next/static (static files)
+    * - _next/image (image optimization files)
+    * - favicon.ico (favicon file)
+    */
+    '/((?!api|_next/static|_next/image|favicon.ico).*)',
+  ],
+};
+
+export default function middleware(request: NextRequest) {
+  let hostname = request.headers.get("host")!;
+
+  // If hostname is either localhost or public domain, then we just redirect to root
+  if (hostname === "localhost:3000" || hostname === process.env.NEXT_PUBLIC_ROOT_DOMAIN) {
+    let path = request.nextUrl.pathname;
+    console.log(path);
+    if(path === "/") {
+        return NextResponse.rewrite(new URL(path, request.url));
+    } else {
+        return NextResponse.rewrite(new URL(`${path}`, request.url));
+    }
+  }
+
+  // Else, we direct whatever.domain.com to domain.com/whatever
+  return NextResponse.rewrite(new URL(`/${hostname.split(".")[0]}`, request.url));
+}
