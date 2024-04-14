22bba51f0f2d6744f723211ab1a6b55989ba60d5
Ryan Liu <ryan@ryanliu6.xyz>
2024-04-13 20:37:59 -0700

    Removing NextJS from main branch in prep of AstroJS

--- app/utils/utils.tsx
+++
@@ -1,9 +0,0 @@
-import { ReactNode } from "react";
-
-export function Conditional(props: { show: boolean, trueChildren: ReactNode, falseChildren: ReactNode }) {
-  if (props.show) {
-    return <>{props.trueChildren}</>;
-  } else {
-    return <>{props.falseChildren}</>;
-  }
-}
