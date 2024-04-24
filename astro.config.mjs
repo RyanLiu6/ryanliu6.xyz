f88c8f1a5077c37e1b2c1d03f95b0652b822c762
Ryan Liu <ryan@ryanliu6.xyz>
2024-04-24 15:15:42 -0700

    Using React instead of Preact

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,9 @@
-import { defineConfig } from 'astro/config';
-import preact from "@astrojs/preact";
-
+import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
+
+import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [preact(), tailwind()]
+  integrations: [react(), tailwind()]
 });
