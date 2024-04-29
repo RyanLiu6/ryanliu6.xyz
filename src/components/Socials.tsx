18a41846fc93099bd009e9b07604be76f159a359
Ryan Liu <ryan@ryanliu6.xyz>
2024-04-28 20:49:33 -0700

    Refactoring BaseLayout.astro and 404.astro

--- src/components/Socials.tsx
+++ src/components/Socials.tsx
@@ -7,28 +7,28 @@ const Socials = () => {
         href="https://www.linkedin.com/in/ryanliu6/"
         title="View my LinkedIn Profile"
       >
-        <img height="32" width="32" src="https://cdn.simpleicons.org/linkedin/gray" />
+        <img height="32" width="32" alt="LinkedIn Icon" src="https://cdn.simpleicons.org/linkedin/gray" />
       </RedirectButton>
 
       <RedirectButton
         href="https://www.github.com/ryanliu6"
         title="View my GitHub Profile"
       >
-        <img height="32" width="32" src="https://cdn.simpleicons.org/github/gray" />
+        <img height="32" width="32" alt="GitHub Icon" src="https://cdn.simpleicons.org/github/gray" />
       </RedirectButton>
 
       <RedirectButton
         href="mailto:ryan@ryanliu6.xyz"
         title="Email me!"
       >
-        <img height="32" width="32" src="https://cdn.simpleicons.org/protonmail/gray" />
+        <img height="32" width="32" alt="Mail Icon" src="https://cdn.simpleicons.org/protonmail/gray" />
       </RedirectButton>
 
       <RedirectButton
         href="/Resume.pdf"
         title="View my Resume"
       >
-        <img height="32" width="32" src="https://cdn.simpleicons.org/libreofficewriter/gray" />
+        <img height="32" width="32" alt="File Icon" src="https://cdn.simpleicons.org/libreofficewriter/gray" />
       </RedirectButton>
     </div>
   )
--- src/components/utils.tsx
+++ src/components/utils.tsx
@@ -11,13 +11,16 @@ export const textClass = classNames("text-slate-600 dark:text-slate-200")
 
 export const backgroundClass = classNames("bg-violet-50/50 dark:bg-slate-900")
 
+export const buttonClass = classNames("flex rounded-lg p-2 transition hover:bg-rose-300/30 hover:dark:bg-zinc-700 hover:shadow-lg")
+
+export const flexClass = classNames("flex flex-col flex-1")
+
+export const shadowBorder = classNames("rounded-full px-6 py-2 shadow-lg bg-rose-300/30 dark:bg-zinc-700");
+
+export const bodyBorder = classNames(flexClass, textClass, backgroundClass, "min-h-screen")
+
+export const mainBorder = classNames(flexClass, "items-center px-6 lg:px-8")
+
 export const baseBorder = classNames("relative mx-auto max-w-screen-lg px-6 py-6");
 
-export const bodyBorder = classNames(baseBorder, "sm:px-6 lg:px-8")
-// export const bodyBorder = classNames(baseBorder, "sm:px-6 lg:px-8 flex flex-col flex-1 items-center")
-
 export const marginalBorder = classNames(baseBorder, textClass);
-
-export const mainBorder = classNames(textClass, backgroundClass, "min-h-screen")
-
-export const shadowBorder = classNames("rounded-full px-6 py-2 shadow-lg bg-fuchsia-200/20 dark:bg-zinc-700");
