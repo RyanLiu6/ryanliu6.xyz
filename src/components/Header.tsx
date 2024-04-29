81d4520ee3c9eb6a31c23c28dad3df776ce7308b
Ryan Liu <ryan@ryanliu6.xyz>
2024-04-28 23:05:54 -0700

    Refactor Header.tsx to also use <section> and removing extraClass props

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,21 +6,23 @@ import ThemeIcon from "@/components/ThemeIcon";
 import { marginalBorder, shadowBorder } from "@/components/utils";
 
 const Header = () => {
-  const headerClass = classNames(marginalBorder, "flex flex-col")
+  const headerClass = classNames(marginalBorder, "flex flex-row w-full relative justify-between")
   const navClass = classNames(shadowBorder, "hidden md:flex")
   const menuClass = classNames(shadowBorder, "flex md:hidden")
 
   return (
-    <header>
-      <div className={headerClass}>
-      <div className="relative flex justify-between">
-        <div className="flex justify-center flex-1">
-          <Navigation extraClass={navClass} />
-          <Menu extraClass={menuClass} />
+    <header className={headerClass}>
+      <section className="flex flex-1 justify-center">
+        <div className={navClass}>
+          <Navigation />
         </div>
+        <div className={menuClass}>
+          <Menu />
+        </div>
+      </section>
+      <section>
         <ThemeIcon />
-      </div>
-    </div>
+      </section>
     </header>
   )
 }
