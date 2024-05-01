d9c30d0ad41844bdf0ec28dc36a1aba2dc7a8781
Ryan Liu <ryan@ryanliu6.xyz>
2024-04-30 20:00:21 -0700

    Adding roboto as a font and title section for work page

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,7 +3,11 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				roboto: ["Roboto"]
+			}
+		},
 	},
 	plugins: [],
 	darkMode: "selector",
