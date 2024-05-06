4f56fdfa5a30e86aea73779fb04688f52363e654
Ryan Liu <ryan@ryanliu6.xyz>
2024-05-05 23:56:16 -0700

    Refactor and changing popover size

--- src/components/Socials.tsx
+++ src/components/Socials.tsx
@@ -7,28 +7,32 @@ const Socials = () => {
         href="https://www.linkedin.com/in/ryanliu6/"
         title="View my LinkedIn Profile"
       >
-        <img height="32" width="32" alt="LinkedIn Icon" src="https://cdn.simpleicons.org/linkedin/gray" />
+        <img height="24" width="24" alt="LinkedIn Icon" src="https://cdn.simpleicons.org/linkedin/475569" className="flex dark:hidden" />
+        <img height="24" width="24" alt="LinkedIn Icon" src="https://cdn.simpleicons.org/linkedin/d4d4d8" className="hidden dark:flex" />
       </RedirectButton>
 
       <RedirectButton
         href="https://www.github.com/ryanliu6"
         title="View my GitHub Profile"
       >
-        <img height="32" width="32" alt="GitHub Icon" src="https://cdn.simpleicons.org/github/gray" />
+        <img height="24" width="24" alt="GitHub Icon" src="https://cdn.simpleicons.org/github/475569" className="flex dark:hidden" />
+        <img height="24" width="24" alt="GitHub Icon" src="https://cdn.simpleicons.org/github/d4d4d8" className="hidden dark:flex" />
       </RedirectButton>
 
       <RedirectButton
         href="mailto:ryan@ryanliu6.xyz"
         title="Email me!"
       >
-        <img height="32" width="32" alt="Mail Icon" src="https://cdn.simpleicons.org/protonmail/gray" />
+        <img height="24" width="24" alt="Mail Icon" src="https://cdn.simpleicons.org/protonmail/475569" className="flex dark:hidden" />
+        <img height="24" width="24" alt="Mail Icon" src="https://cdn.simpleicons.org/protonmail/d4d4d8" className="hidden dark:flex" />
       </RedirectButton>
 
       <RedirectButton
         href="/Resume.pdf"
         title="View my Resume"
       >
-        <img height="32" width="32" alt="File Icon" src="https://cdn.simpleicons.org/libreofficewriter/gray" />
+        <img height="24" width="24" alt="File Icon" src="https://cdn.simpleicons.org/libreofficewriter/475569" className="flex dark:hidden" />
+        <img height="24" width="24" alt="File Icon" src="https://cdn.simpleicons.org/libreofficewriter/d4d4d8" className="hidden dark:flex" />
       </RedirectButton>
     </>
   )
--- src/components/common/utils.tsx
+++ src/components/common/utils.tsx
@@ -13,13 +13,13 @@ export const mdClass = classNames(textClass, "prose dark:prose-invert max-w-none
 
 export const backgroundClass = classNames("bg-violet-50/50 dark:bg-slate-900")
 
-export const buttonClass = classNames("flex rounded-lg p-2 transition hover:bg-rose-300/30 hover:dark:bg-zinc-700 hover:shadow-lg")
+export const buttonClass = classNames("flex rounded-lg p-2 transition hover:bg-rose-300/30 hover:dark:bg-fuschia-700 hover:shadow-lg")
 
 export const linkClass = classNames("font-bold", "transition", "hover:text-cyan-500")
 
 export const flexClass = classNames("flex flex-col flex-1")
 
-export const shadowBorder = classNames("rounded-full px-6 py-2 shadow-lg bg-rose-300/30 dark:bg-zinc-700");
+export const shadowBorder = classNames("rounded-full px-6 py-2 shadow-lg bg-rose-300/30 dark:bg-fuschia-700");
 
 export const bodyBorder = classNames(flexClass, textClass, backgroundClass, "min-h-screen")
 
