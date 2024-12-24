f554c8bb93ceddeef8559e90a0bdbdf2ff9b0e39
Ryan Liu <ryanliu6@pm.me>
2024-12-23 21:11:10 -0800

    Updating @ryanliu6/xi to v2.2.0 and fixing tailwind config

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,7 +1,7 @@
 import typography from "@tailwindcss/typography";
 
 export default {
-  content: ["./src/**/*.{js,jsx,ts,tsx}", "./node_modules/@ryanliu6/xi/dist/**/*.js"],
+  content: ["./src/**/*.{js,jsx,ts,tsx,astro}", "./node_modules/@ryanliu6/xi/dist/**/*.js"],
   theme: {
     extend: {
       fontFamily: {
