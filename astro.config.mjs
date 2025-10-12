38b8d4dd9bba57cc428427d2a2308708756a66f3
Ryan Liu <14251414+RyanLiu6@users.noreply.github.com>
2025-10-12 14:05:43 -0700

    Refactor for tailwind v4 (#2)
    This change does:
    * Switches to pnpm
    * Upgrades to tailwind v4 properly

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
-import react from "@astrojs/react";
-import tailwind from "@astrojs/tailwind";
-
 import { defineConfig } from "astro/config";
+import react from "@astrojs/react";
+import tailwindcss from "@tailwindcss/vite";
 
-// https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind()],
+  integrations: [react()],
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
